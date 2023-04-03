68ef3a13f091fdf9e9c128e534240682ad1be93d
Egemen Sevgi <egemen2001sevgi@hotmail.com>
2023-04-03 19:51:21 +0300

    added new website to scrap

--- main.py
+++ main.py
@@ -49,11 +49,11 @@ class CryptoJobs:  # https://cryptojobslist.com/
                 return
 
         with open("Data/crypto_jobs.txt", "a", encoding="utf-8") as f:
-            f.write(f"Title: {title}\n")
-            f.write(f"Description: {desc}\n")
-            f.write(f"Link: {link}\n")
+            f.write(f"Title:\n {title}\n")
+            f.write(f"Description:\n {desc}\n")
+            f.write(f"Link:\n {link}\n")
             f.write("--------------------------\n")
-            print("------------------------------- YENİ HABERLERİNİZ MEVCUT--------------------")
+            print("------------------------------- YENİ HABERLERİNİZ MEVCUT(https://cryptojobslist.com/)--------------------")
             print(f"------------İLAN BAŞLIĞI ----------------\n")
             print(title + " at " + where + "\n")
             print(f"------------İLAN BAŞLIĞI SONU ----------------\n")
@@ -64,7 +64,7 @@ class CryptoJobs:  # https://cryptojobslist.com/
             return
 
 
-class Web3Jobs: # https://crypto.jobs/
+class Web3Jobs:  # https://crypto.jobs/
     def __init__(self):
 
         self.options = webdriver.ChromeOptions()
@@ -107,11 +107,11 @@ class Web3Jobs: # https://crypto.jobs/
                 return
 
         with open("Data/web3_jobs.txt", "a", encoding="utf-8") as f:
-            f.write(f"Title: {title}\n")
-            f.write(f"Description: {desc}\n")
-            f.write(f"Link: {link}\n")
+            f.write(f"Title:\n {title}\n")
+            f.write(f"Description:\n {desc}\n")
+            f.write(f"Link:\n {link}\n")
             f.write("--------------------------\n")
-            print("------------------------------- YENİ HABERLERİNİZ MEVCUT--------------------")
+            print("------------------------------- YENİ HABERLERİNİZ MEVCUT (https://crypto.jobs/)--------------------")
             print(f"------------İLAN BAŞLIĞI ----------------\n")
             print(title)
             print(f"------------İLAN BAŞLIĞI SONU ----------------\n")
@@ -122,8 +122,7 @@ class Web3Jobs: # https://crypto.jobs/
             return
 
 
-class Web3Career: #https://web3.career/
-
+class Web3Career:  # https://web3.career/
 
     def goToSite(self, url, page):
         import requests
@@ -144,21 +143,21 @@ class Web3Career: #https://web3.career/
                 description_text = description_html.text_content()
             else:
                 description_text = "N/A"
-            self.write_to_file(title, apply_url, description_text)
+            self.write_to_file(title, description_text, apply_url)
             print(f"Title: {title}\nApply URL: {apply_url}\nDescription: {description_text}\n")
 
     def write_to_file(self, title, desc, link):
         with open("Data/web3_career.txt", "r", encoding="utf-8") as f:
-            if link in f.read():
+            if desc in f.read():
                 print(f"\n\nhttps://web3.career/ İÇİN YENİ BİR İLAN YOK.")
-                sys.exit()
+                CryptoCurrencyJobs().goToSite("https://cryptocurrencyjobs.co/")
 
         with open("Data/web3_career.txt", "a", encoding="utf-8") as f:
-            f.write(f"Title: {title}\n")
-            f.write(f"Description: {desc}\n")
+            f.write(f"Title:\n {title}\n")
+            f.write(f"Description:\n {desc}\n")
             f.write(f"Link: {link}\n")
             f.write("--------------------------\n")
-            print("------------------------------- YENİ HABERLERİNİZ MEVCUT--------------------")
+            print("------------------------------- YENİ HABERLERİNİZ MEVCUT (https://web3.career/)--------------------")
             print(f"------------İLAN BAŞLIĞI ----------------\n")
             print(title)
             print(f"------------İLAN BAŞLIĞI SONU ----------------\n")
@@ -166,6 +165,59 @@ class Web3Career: #https://web3.career/
             print(desc + "\n")
             print("İLANIN LİNKİ ::::::: " + link)
             print(f"------------İLAN AÇIKLAMASI SONU ----------------\n")
+
+
+class CryptoCurrencyJobs:  # https://cryptocurrencyjobs.co/
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.headless = True  # selenium browser kapatmak için bu özelliği True işaretlemek zorundayız.
+        self.driver = webdriver.Chrome(options=self.options)
+
+    def goToSite(self, url):
+        self.driver.get(f"{url}")
+        time.sleep(2)
+        items = self.driver.find_elements_by_xpath('//div//h2//a')
+        for i in range(len(items)):
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            wait = WebDriverWait(self.driver, 10)
+            wait.until(EC.element_to_be_clickable(
+                (By.XPATH, '//div//h2//a')))
+            button_elements = self.driver.find_elements(By.XPATH, '//div//h2//a')
+            button_elements[i].click()
+            job_title_element = wait.until(
+                EC.presence_of_element_located((By.XPATH, '//main//div//div//h1')))
+            title = job_title_element.text
+            job_desc_element = wait.until(
+                EC.presence_of_element_located((By.XPATH, '/html/body/main/div/div[2]/div/div[1]/div[1]')))
+            desc = job_desc_element.text
+            self.write_to_file(title,desc,self.driver.current_url)
+            time.sleep(0.45)
+            self.driver.back()
+            time.sleep(0.45)
+
+    def write_to_file(self, title, desc, link):
+        with open("Data/crypto_currency_jobs.txt", "r", encoding="utf-8") as f:
+            if desc in f.read():
+                print(f"\n\nhttps://cryptocurrencyjobs.co/ İÇİN YENİ BİR İLAN YOK.")
+                self.driver.quit()
+                sys.exit(1)
+
+        with open("Data/crypto_currency_jobs.txt", "a", encoding="utf-8") as f:
+            f.write(f"Title:\n {title}\n")
+            f.write(f"Description:\n {desc}\n")
+            f.write(f"Link: {link}\n")
+            f.write("--------------------------\n")
+            print("------------------------------- YENİ HABERLERİNİZ MEVCUT (https://cryptocurrencyjobs.co/)--------------------")
+            print(f"------------İLAN BAŞLIĞI ----------------\n")
+            print(title)
+            print(f"------------İLAN BAŞLIĞI SONU ----------------\n")
+            print(f"------------İLAN AÇIKLAMASI ----------------\n")
+            print(desc + "\n")
+            print("İLANIN LİNKİ ::::::: " + self.driver.current_url)
+            print(f"------------İLAN AÇIKLAMASI SONU ----------------\n")
+            return
+
+
 
 
 def first_website():
@@ -179,4 +231,3 @@ def second_website():
 
 
 first_website()
-
